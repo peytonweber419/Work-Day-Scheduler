919ae526b4d1d0054d509777453cdb59c62a207b
Peyton Weber <weberpeyton11@gmail.com>
2023-10-27 17:41:49 -0500

    Added comments to the code.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Display today's date
+// Displays the current date
 var todaysDate = dayjs().format('dddd, MMM D YYYY');
 $ ("#currentDay").html(todaysDate);
 
@@ -6,23 +6,16 @@ $ ("#currentDay").html(todaysDate);
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
+
+    // Creates the event listener for clicking the save button and adds the description to local storage.
+
     $ (".saveBtn") .click (function(){
       var id = $ (this) .parent() .attr("id");
       var task = $ (this) .siblings(".description") .val();
       localStorage.setItem(id, task);
     })
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+
+    // Refers to each time block and decides whether or not it is in the past present or future.
     
     $ (".time-block") .each (function(){
       var currentHour = dayjs().hour();
@@ -38,9 +31,9 @@ $(function () {
       }
     })
 
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
+    // Creates a for loop that starts at the first time block and stops after the last.
+    // Appends the hour and number of the hour to the page from local storage using getItem.
+
     for (var i = 9; i < 18; i++) {
       $ ("#hour-" + i + " .description") .val (localStorage.getItem("hour-" + i));
     }
